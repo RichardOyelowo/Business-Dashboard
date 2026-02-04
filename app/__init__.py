3b4836eb8472607dc97a61a8732a12f036ade1e0
Richard <richardadebowale.oye@gmail.com>
2026-02-04 17:14:04 -0500

    initialisation pre-final check

---
+++ app/__init__.py
@@ -0,0 +1,29 @@
+import os
+from flask import Flask
+from app.config import Config
+from dotenv import load_dotenv
+from app.extensions import db, migrate
+from app.auth import register_auth
+from app.routes import register_routes
+from app.config import DevelopmentConfig, ProductionConfig
+
+
+load_dotenv()
+
+def create_app():
+    app = Flask(__name__)
+
+    env = os.environ.get("FLASK_ENV", "development")
+
+    if env == "production":
+        app.config.from_object(ProductionConfig)
+    else:
+        app.config.from_object(DevelopmentConfig)
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    register_routes(app)
+    register_auth(app)
+
+    return app
